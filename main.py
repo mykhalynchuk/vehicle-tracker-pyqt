d33548b51ce501d4ceab2b916be9785a8961e302
Oksana <oksanamikhalyncuk@gmail.com>
2026-04-18 18:19:57 +0300

    feat: add VideoThread for background video processing and car detection

--- main.py
+++ main.py
@@ -2,12 +2,91 @@ import sys
 import cv2
 import os
 
+import numpy as np
 from PyQt6.QtMultimedia import QMediaPlayer
 from PyQt6.QtMultimediaWidgets import QVideoWidget
 from PyQt6.QtCore import QThread, pyqtSignal, Qt
 from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QFileDialog, QApplication, QHBoxLayout, QSlider
+from PyQt6.QtWidgets.QWidget import height
 
+
+class VideoThread(QThread):
+    resultSignal = pyqtSignal(object)
+
+    def __init__(self, videoPath):
+        super().__init__()
+        self.videoPath = videoPath
+        self.running = True
+        self.pauseThread = False
+
+    def run(self):
+        video = cv2.VideoCapture(self.videoPath)
+
+        if not video.isOpened():
+            return
+
+        subtractorBackground =cv2.createBackgroundSubtractorMOG2(
+            history=500,
+            varThreshold=80,
+            detectShadows=True)
+
+        kernelOpen = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        kernelClose = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 7))
+
+        while self.running:
+
+            if self.pauseThread:
+                self.msleep(50)
+                continue
+
+            ret, frame = video.read()
+
+            if not ret:
+                break
+
+            height, width = frame.shape[:2]
+            maskRoi = np.zeros((height, width), dtype=np.uint8)
+            cv2.rectangle(maskRoi, (0, int(height * 0.4)), (width, height), 255, -1)
+            roiFrame = cv2.bitwise_and(frame, frame, mask=maskRoi)
+
+            grayFrame = cv2.cvtColor(roiFrame, cv2.COLOR_BGR2GRAY)
+            blurFrame = cv2.GaussianBlur(grayFrame, (5, 5), 0)
+
+            fgMask = subtractorBackground.apply(blurFrame, learningRate=0.01)
+            _, fgMask = cv2.threshold(fgMask, 240, 255, cv2.THRESH_BINARY)
+
+            fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_OPEN, kernelOpen, iterations=1)
+            fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_CLOSE, kernelClose, iterations=3)
+
+            contours, _ = cv2.findContours(fgMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > 300:
+                    x, y, w, h = cv2.boundingRect(contour)
+
+                    ratio = w/h
+                    if 0.4 <  ratio < 4.0:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        cv2.putText(frame, "Car", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            if ret:
+                self.resultSignal.emit(frame)
+
+            self.msleep(30)
+
+
+        video.release()
+
+    def pause(self):
+        self.pauseThread = not self.pauseThread
+
+
+    def stop(self):
+        self.running = False
+        self.quit()
+        self.wait()
 
 class App(QWidget):
     def __init__(self):
